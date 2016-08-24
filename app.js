89ae794d31b1f1ea1effc0ebb88e79867b70d09a
rahul <pandeyrahul2k8@gmail.com>
2016-08-24 16:38:17 +0530

    Removed heat map functionality and modified bubble size based on strength

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([-37.87, 175.475], 10);
+var map = L.map('map').setView([-37.87, 175.475], 14);
     mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 
 //Heat--------------------------------------------------------------
@@ -6,19 +6,36 @@ L.tileLayer(
     'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; ' + mapLink + ' Contributors',
     maxZoom: 18,
+    minZoom: 12
 }).addTo(map);
 
-var heat = L.heatLayer(addressPoints,{
+/*var heat = L.heatLayer(addressPoints,{
     radius: 20,
     blur: 15, 
     maxZoom: 17,
-}).addTo(map);
+}).addTo(map);*/
 
 
 // Bubble----------------------------------------------------------
 
 var markers = L.markerClusterGroup({
-    maxClusterRadius: 40,
+    maxClusterRadius: 120,
+    iconCreateFunction: function(cluster) {
+      var childCount = cluster.getChildCount();
+      var c = ' marker-cluster-';
+      var x, y;
+      if (childCount < 10) {
+        c += 'small';
+        x = 30, y = 30;
+      } else if (childCount < 100) {
+        c += 'medium';
+        x = 60, y = 60;
+      } else {
+        c += 'large';
+        x = 90, y = 90;
+      }
+    return new L.DivIcon({ html: '<div class="child-count"><span>'+childCount+'</span></div>', className: 'marker-cluster' + c, iconSize: new L.Point(x, y) });
+    },
     //Disable all of the defaults:
     spiderfyOnMaxZoom: false, showCoverageOnHover: false, zoomToBoundsOnClick: true
     
